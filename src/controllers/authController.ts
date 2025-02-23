707c3c1e4bb45729d3621a67e252881da0768038
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-02-23 14:53:26 +0200

    Removed license file

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from 'express';
 import { SUCCESS } from '../constants/responseConstants';
 
 import errorHandler from "../utils/asyncErrorHandler";
-import APIError from '../types/classes/APIError';
 
 export const login = errorHandler(async (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
