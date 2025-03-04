35382735a4facd5a80c9e705d42399290544c002
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-03-04 18:22:32 +0200

    Implemented signup and email confirmation endpoints + docs

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,11 +1,81 @@
 import { Request, Response, NextFunction } from 'express';
 import { SUCCESS } from '../constants/responseConstants';
-
 import errorHandler from "../utils/asyncErrorHandler";
+import {
+  getUser,
+  createAuthRecord,
+  createUser,
+  updateUserAuthCredentials 
+  } from '../services/userService';
+import APIError from '../types/classes/APIError';
+import bcrypt from 'bcrypt';
+import { Role } from '@prisma/client';
+import transporter from '../utils/mail/mailSender';
+import { confirmationCodeTemplate } from '../utils/mail/mailTemplates';
 
 export const login = errorHandler(async (req: Request, res: Response, next: NextFunction) => {
+
   res.status(200).json({
     status: SUCCESS,
     message: 'Successfully logged in.'
+  });
+});
+
+export const signup = errorHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+  const {  name, id, email, password, country, role } = req.body;
+  if (await getUser({ searchBy: { id } })) {
+    return next(new APIError(400, `${id} has already been taken`));
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const roleENUM = role === 'mentee' ? Role.MENTEE : Role.MENTOR;
+
+  // this statement will mislead the user that are trying to figure out if an email is already taken
+  if (await getUser({ searchBy: { email } })){
+    res.json({
+      status: SUCCESS,
+      message: `Registered successfully, a confirmation code has been sent to ${email}`
+    });
+    return;
+  }
+
+  // create a 6 digit confirmation code (starting from 000000 to 999999)
+  const confirmationCode = `${Math.ceil((Math.random() * 999999))}`.padStart(6, '0');
+  console.log(confirmationCode);
+  await createUser({
+    name,
+    id,
+    email,
+    password: hashedPassword,
+    country,
+    role: roleENUM,
+  });
+  await createAuthRecord({
+    userId: id,
+    confirmationCode,
+  });
+  await transporter.sendMail(confirmationCodeTemplate(name, email, confirmationCode));
+    res.status(201).json({
+    status: SUCCESS,
+    message: `Registered successfully, a confirmation code has been sent to ${email}`
+  });
+});
+
+export const confirmEmail = errorHandler(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, code } = req.body;
+  const user = await getUser({ searchBy: { email }, authCredentials: true });
+  if (!user) {
+    return next(new APIError(400, 'Invalid email or confirmation code'));
+  }
+  if (user.authCredentials?.emailVerified === true){
+    return next(new APIError(400, 'Email has already been verified'));
+  }
+  if (user.authCredentials?.emailVerificationCode !== code){
+    return next(new APIError(400, 'Invalid confirmation code'));
+  }
+  await updateUserAuthCredentials(user.id, { emailVerified: true, emailVerificationCode: null });
+  res.status(200).json({
+    status: SUCCESS,
+    message: `Email ${user.email} has been verified successfully`,
   });
 });
--- src/routes/v1/authRouter.ts
+++ src/routes/v1/authRouter.ts
@@ -1,8 +1,9 @@
-// example router
-
 import { Router } from 'express';
 import { notAllowedMethod, notFoundEndpoint } from '../../middlewares/notAllowedHandler';
-import { login } from '../../controllers/authController';
+import { login, signup, confirmEmail } from '../../controllers/authController';
+import requestValidator from '../../middlewares/requestValidator';
+import { confirmEmailSchema, signupSchema } from '../../validators/validate.auth';
+import { rateLimiter } from '../../utils/rateLimiter';
 
 const authRouter = Router();
 
@@ -10,6 +11,19 @@ authRouter.route('/login')
   .post(login)
   .all(notAllowedMethod);
 
+authRouter.route("/signup")
+  .post(
+    requestValidator({ bodySchema: signupSchema }),
+    signup
+  )
+  .all(notAllowedMethod);
+
+authRouter.route("/confirm-email")
+  .post(
+    rateLimiter(60, 5, "Too many requests, please try again later"),
+    requestValidator({ bodySchema: confirmEmailSchema }),
+    confirmEmail
+  )
 authRouter.route('*').all(notFoundEndpoint);
 
 export default authRouter;
