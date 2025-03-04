f40e4d02bdde70d48afdc8d71ae82ffff000b3fe
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-03-04 18:20:52 +0200

    Implemented custom rate limiter

---
+++ src/utils/rateLimiter.ts
@@ -0,0 +1,9 @@
+import rateLimit from "express-rate-limit";
+
+export const rateLimiter = (minutes: number, max: number, message: string) => {
+  return rateLimit({
+    windowMs: minutes * 60000,
+    max,
+    message,
+  });
+};
