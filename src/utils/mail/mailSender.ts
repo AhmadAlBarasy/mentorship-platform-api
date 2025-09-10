447b0dfdcf1baff519e62729c39ee2a25dc7f265
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-09-10 16:31:09 +0200

    used resend for mail sending

--- src/utils/mail/mailSender.ts
+++ src/utils/mail/mailSender.ts
@@ -1,54 +1,21 @@
-import { google } from 'googleapis';
 import dotenv from 'dotenv';
-import { SendMailOptions } from 'nodemailer';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 dotenv.config({ path: './config/.env' });
 
-const oAuth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID!,
-  process.env.GOOGLE_CLIENT_SECRET!,
-  process.env.GOOGLE_REDIRECT_URI!,
-);
+async function sendEmail(content: any) {
 
-oAuth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN!,
-});
-
-const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
-
-function createRawTextMessage({ from, to, subject, text }: SendMailOptions): string {
-  const message = [
-    `From: ${from}`,
-    `To: ${to}`,
-    `Subject: ${subject}`,
-    'MIME-Version: 1.0',
-    'Content-Type: text/plain; charset="UTF-8"',
-    'Content-Transfer-Encoding: 7bit',
-    '',
-    text || '',
-  ].join('\n');
-
-  return Buffer.from(message)
-    .toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=+$/, '');
-}
-
-async function sendEmail(content: SendMailOptions) {
-  const rawMessage = createRawTextMessage({
-    from: content.from || `Growthly Platform <${process.env.GMAIL_ADDRESS}>`,
-    to: content.to,
+  const res =  await resend.emails.send({
+    from: content.from,
+    to: 'growtlyp@gmail.com',
     subject: content.subject,
-    text: content.text as string,
+    text: content.text,
   });
 
-  await gmail.users.messages.send({
-    userId: 'me',
-    requestBody: {
-      raw: rawMessage,
-    },
-  });
+  console.log(res);
+
 }
 
 export default sendEmail;
