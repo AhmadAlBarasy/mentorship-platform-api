447cc4a8bb6e457ad12156421b7ba309a6cc210b
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-09-11 18:37:57 +0200

    rate limited the endpoint

--- src/routes/v1/dashboardRouter.ts
+++ src/routes/v1/dashboardRouter.ts
@@ -5,12 +5,17 @@ import { notAllowedMethod } from '../../middlewares/notAllowedHandler';
 import { Role } from '@prisma/client';
 import requestValidator from '../../middlewares/requestValidator';
 import { searchQuerySchema } from '../../validators/validate.common';
+import rateLimit from 'express-rate-limit';
 
 const dashboardRouter = Router();
 const { MENTEE } = Role;
 
 dashboardRouter.route('/search')
   .get(
+    rateLimit({
+      windowMs: 1000,
+      limit: 50,
+    }),
     authenticate({ access: 'full' }),
     requestValidator({ querySchema: searchQuerySchema }),
     searchUsersAndCommunities,
