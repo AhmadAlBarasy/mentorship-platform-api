2f0dbccae6af7b5a51fd04490303c800bfcf75ea
Ahmad Albarasy <ahmadbarasy@gmail.com>
2025-10-03 00:48:05 +0200

    allowed logs to the console in prod + temporarily removed file logs

--- src/loggers/httpLogger.ts
+++ src/loggers/httpLogger.ts
@@ -9,14 +9,9 @@ const cliFormat =  printf(({ level, message, timestamp }) => {
 const httpLogger = winston.createLogger({
   level: 'http', // change based on your needs or use your own severity system
   format: combine(timestamp(), json()),
-  // transports: [
-  //   new winston.transports.File({ filename: './logs/http.log' }),
-  // ]
 });
 
 // log HTTP requests to the console if the node environment is 'development'
-if (process.env.NODE_ENV !== 'production') {
-  httpLogger.add(new winston.transports.Console({ format: combine(timestamp(), cliFormat) }));
-}
+httpLogger.add(new winston.transports.Console({ format: combine(timestamp(), cliFormat) }));
 
 export default httpLogger;
